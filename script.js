a62c8bd397e3bc2bc335d0a07db715e36c459b12
zbracko <96391637+zbracko@users.noreply.github.com>
2023-09-20 00:49:51 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const circle = document.querySelector('.circle');
+    const circleText = document.querySelector('.circle-text');
+    
+    // Spin the circle for 1 second
+    circle.style.animation = 'spin 1s linear';
+
+    setTimeout(() => {
+        // Stop the spin and start typing
+        circle.style.animation = '';
+        typeText("Hello, welcome!", circleText);
+    }, 1000);
+});
+
+function typeText(text, element) {
+    let index = 0;
+
+    function type() {
+        if (index < text.length) {
+            element.textContent += text[index];
+            index++;
+            setTimeout(type, 100);
+        }
+    }
+
+    type();
+}
